2d1a9d634d45cce497558bf18484d36acccb26b2
FinixLei <finixlei@gmail.com>
2014-11-06 09:37:16 +0800

    Create new package to replace default package

---
+++ src/HelloJava/GenOne.java
@@ -0,0 +1,34 @@
+package HelloJava;
+
+class GenOne {
+	public int data;
+	
+	public GenOne(){
+		System.out.println("In GenOne Constructor");
+	}
+	
+	protected void doSomething(){
+		System.out.println("In GenOne: do something!");
+	}
+	
+	protected GenOne doIt(){
+		return new GenOne();
+	}
+	
+	public void setData(int data){
+		this.data = data;
+	}
+	
+	public int getData(){
+		return this.data;
+	}
+	
+	public boolean equals(GenOne other){
+		if (this.data == other.data){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
